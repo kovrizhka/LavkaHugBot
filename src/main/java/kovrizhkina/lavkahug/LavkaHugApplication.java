ff843793ae8bc89a9ddf47f59667c195f4e06266
MrSilver <pivsru@gmail.com>
2024-07-07 11:45:21 +0500

    first commit

---
+++ src/main/java/kovrizhkina/lavkahug/LavkaHugApplication.java
@@ -0,0 +1,13 @@
+package kovrizhkina.lavkahug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavkaHugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavkaHugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kovrizhkina/lavkahug/LavkaHugApplicationTests.java
@@ -0,0 +1,13 @@
+package kovrizhkina.lavkahug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavkaHugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
