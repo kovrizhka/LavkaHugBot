f258ac83285168128d2e7ddbeab3139c5ff16be4
MrSilver <pivsru@gmail.com>
2024-09-25 21:38:54 +0500

    добавлена отправка сообщений в брокер rabbitmq

---
+++ dispatcher/src/main/java/ru/lavka/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,35 @@
+package ru.lavka.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static ru.lavka.model.RabbitQueue.*;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue docMessageQueue() {
+        return new Queue(DOC_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue answerMessageQueue() {
+        return new Queue(ANSWER_MESSAGE);
+    }
+
+
+}
